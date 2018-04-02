a13869f26e1bee44b62a19f1a620c8524da2cd10
Leonardo Goulart <leonardogoulartt1@gmail.com>
2018-04-02 16:18:59 -0300

    DDP API v201802 update

--- ddpclient/client.py
+++ ddpclient/client.py
@@ -5,8 +5,8 @@ import os
 import httplib2
 
 USER_AGENT = 'DDP API Call'
-USER_LIST_SERVICE_WSDL_URL = 'https://ddp.googleapis.com/api/ddp/provider/v201705/UserListService?wsdl'
-USER_LIST_CLIENT_SERVICE_WSDL_URL = 'https://ddp.googleapis.com/api/ddp/provider/v201705/UserListClientService?wsdl'
+USER_LIST_SERVICE_WSDL_URL = 'https://ddp.googleapis.com/api/ddp/provider/v201802/UserListService?wsdl'
+USER_LIST_CLIENT_SERVICE_WSDL_URL = 'https://ddp.googleapis.com/api/ddp/provider/v201802/UserListClientService?wsdl'
 
 
 class Client(object):
